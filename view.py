b770548cbabf3cf1cabe862663dac0679ed44d85
Ekaterina Ronzhina <shumilovaek1986@gmail.ru>
2022-11-14 09:34:45 +0300

    DZ

--- view.py
+++ view.py
@@ -4,4 +4,4 @@ from create_bot import bot
 
 async def hello(message: types.Message):
     await bot.send_message(message.from_user.id, f'{message.from_user.first_name}, привет!'
-                                                 f'Сегодня будем делить конфеты')
+                                                 f'Сегодня будем делить конфеты. Выбери уровень игры: /level 1 - сложный, /level 0 - легкий ')
