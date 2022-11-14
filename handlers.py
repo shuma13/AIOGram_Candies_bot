3cd0c70ba760edf0854e30dc8d6f9df08c37a09e
Ekaterina Ronzhina <shumilovaek1986@gmail.ru>
2022-11-14 09:33:32 +0300

    DZ

--- handlers.py
+++ handlers.py
@@ -6,4 +6,5 @@ import commands
 def registred_handlers(dp: Dispatcher):
     dp.register_message_handler(commands.start, commands=['start'])
     dp.register_message_handler(commands.set_total_candies, commands=['set'])
-    # dp.register_message_handler(commands.player_turn)
+    dp.register_message_handler(commands.set_max_take, commands=['step'])
+    dp.register_message_handler(commands.set_level, commands=['level'])
